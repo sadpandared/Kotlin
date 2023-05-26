3f6e8f5c16542398abbfc3ffbacd517b30d4f45a
sadpandared <sadpandaredd@gmail.com>
2023-05-26 21:48:20 +0300

    lab5

---
+++ lab5/src/test/kotlin/ua/kpi/its/lab/rest/RestApplicationTests.kt
@@ -0,0 +1,13 @@
+package ua.kpi.its.lab.rest
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class RestApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
